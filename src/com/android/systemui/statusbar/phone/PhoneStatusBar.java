c91cdcf26747ef854437c2d8bc671d2402014929
wyc15002105071 <747550962@qq.com>
2021-01-19 09:14:55 +0800

    add Navigation Control

--- src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -702,9 +702,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // TODO: use MediaSessionManager.SessionListener to hook us up to future updates
         // in session state
 
-        //showNavigationBar();
+        showNavigationBar();
         //addNavigationBar();
-        //closeNavigationBar();
+        closeNavigationBar();
         // Lastly, call to the icon policy to install/update all the icons.
         mIconPolicy = new PhoneStatusBarPolicy(mContext, mIconController, mCastController,
                 mHotspotController, mUserInfoController, mBluetoothController,
